6369c98ef47a265e0119c35e1969305687bc9cd7
w35l3y <w35l3y@brasnet.org>
2014-05-08 17:48:26 -0300

    Neopets : Avatars Flash Games [BETA]
    Updated relative paths

--- scripts/127882.user.js
+++ scripts/127882.user.js
@@ -23,7 +23,7 @@
 // @resource       includes http://pastebin.com/download.php?i=eArANXdm
 // @resource       meta https://github.com/w35l3y/userscripts/raw/neopets/scripts/127882.user.js
 // @resource       i18n http://pastebin.com/download.php?i=ULrVTsSg
-// @resource       updaterWindowHtml ../../master/resources/html/updaterWindowHtml
+// @resource       updaterWindowHtml ../../../master/resources/html/updaterWindowHtml
 // @resource       updaterWindowCss http://pastebin.com/download.php?i=C1qAvAed
 // @resource       css_colorbox http://www.neopets.com/games/css/colorbox.css?v=1
 // @resource       avatarPopupHtml http://pastebin.com/download.php?i=RG4TiCqD
@@ -33,13 +33,13 @@
 // @resource       css_gamesroom http://www.neopets.com/games/css/gamesroom_redux.css?v=2
 // @require        http://pastebin.com/download.php?i=sin7DHJi
 // @require        http://pastebin.com/download.php?i=BjjHSA30
-// @require        ../../master/includes/63808.user.js
-// @require        ../../master/includes/56489.user.js
-// @require        ../../master/includes/69584.user.js
-// @require        ../../master/includes/85618.user.js
-// @require        ../../master/includes/87940.user.js
-// @require        ../../master/includes/87942.user.js
-// @require        ../../master/includes/85450.user.js
+// @require        ../../../master/includes/63808.user.js
+// @require        ../../../master/includes/56489.user.js
+// @require        ../../../master/includes/69584.user.js
+// @require        ../../../master/includes/85618.user.js
+// @require        ../../../master/includes/87940.user.js
+// @require        ../../../master/includes/87942.user.js
+// @require        ../../../master/includes/85450.user.js
 // @require        ../includes/127696.user.js
 // @require        http://images.neopets.com/js/jquery-1.7.1.min.js?v=1
 // @require        http://images.neopets.com/js/jquery.colorbox.min.js?v=1
