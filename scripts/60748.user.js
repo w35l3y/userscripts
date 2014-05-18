00d856288d9b0a25d54c633290d31cc66e432a4a
w35l3y <w35l3y@brasnet.org>
2014-05-17 23:16:39 -0300

    Neopets : Stock Helper

--- scripts/60748.user.js
+++ scripts/60748.user.js
@@ -21,15 +21,15 @@
 // @grant          GM_xmlhttpRequest
 // @grant          GM_getResourceText
 // @resource       includes http://pastebin.com/download.php?i=eArANXdm
-// @resource       meta https://github.com/w35l3y/userscripts/raw/neopets/scripts/60748.user.js
+// @resource       meta https://github.com/w35l3y/userscripts/raw/master/scripts/Neopets_Stock_Helper/60748.user.js
 // @resource       i18n http://pastebin.com/download.php?i=ULrVTsSg
-// @resource       updaterWindowHtml ../../../raw/master/resources/html/updaterWindowHtml
+// @resource       updaterWindowHtml ../resources/html/updaterWindowHtml
 // @resource       updaterWindowCss http://pastebin.com/download.php?i=C1qAvAed
-// @require        ../../../raw/master/includes/63808.user.js
-// @require        ../../../raw/master/includes/56489.user.js
-// @require        ../../../raw/master/includes/85618.user.js
-// @require        ../../../raw/master/includes/87940.user.js
-// @require        ../../../raw/master/includes/87942.user.js
+// @require        ../includes/63808.user.js
+// @require        ../includes/56489.user.js
+// @require        ../includes/85618.user.js
+// @require        ../includes/87940.user.js
+// @require        ../includes/87942.user.js
 // @require        http://pastebin.com/download.php?i=P6VTBRRK
 // @history        2.0.0 Fixed some bugs
 // @history        1.0.3.0 Firefox 4 bug fixes
