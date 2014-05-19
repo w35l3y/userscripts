a37c53493a65fabc234b7deb60f5e40f19cec97b
w35l3y <w35l3y@brasnet.org>
2014-05-19 04:19:33 -0300

    Added list of topics (issues)

--- scripts/GitHub_Missing_Readme/README-template.md
+++ scripts/GitHub_Missing_Readme/README-template.md
@@ -5,8 +5,17 @@
 **Summary:** %{value/}<br />%{/description}%{license}
 **License:** %{value/}<br />%{/license}%{/meta}
 [![PayPal - The safer, easier way to pay online!](https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif "PayPal - The safer, easier way to pay online!")](http://goo.gl/Fv19S)
-%{sshots|>1}
+%{sshots|>0}
 ### Screenshots
 %{values}![%{name/}](%{node.textContent/})
 %{/values}%{/sshots}
+%{issues|>0}
+### Issues
+State|Topic|Replies|Author|Updated
+:---:|:---|:---:|:---:|---:
+%{values}%{state/}|#%{number/}: [%{title/}](%{html_url/})|%{comments/}|[%{user.login/}](%{user.html_url/})|%{updated_at/}
+%{/values}%{/issues}
+### Installs
 ![Daily installs](http://gm.wesley.eti.br/count.php?id=%{info.Path/}/%{file.textContent/}&type=image)
+
+This file was generated automatically at `%{updated_at/}`
--- scripts/GitHub_Missing_Readme/main.user.js
+++ scripts/GitHub_Missing_Readme/main.user.js
@@ -7,7 +7,7 @@
 // @copyright   2014+, w35l3y (http://gm.wesley.eti.br)
 // @license     GNU GPL
 // @homepage    http://gm.wesley.eti.br
-// @version     1.1.1
+// @version     1.4.0
 // @language    en
 // @include     http*://github.com/*/userscripts/tree/*/scripts*
 // @icon        http://gm.wesley.eti.br/icon.php?desc=scripts/GitHub_Missing_Readme/main.user.js#
@@ -27,43 +27,54 @@
 
 //GM_setValue("oauth", ""); // https://github.com/settings/tokens/new (execute this row once)
 
-if (/^\/(\w+)\/(\w+)\/tree\/(\w+)\/(.+)/.test(location.pathname)) {
+if (/^\/(\w+)\/(\w+)\/tree\/(\w+)\/(.+)/.test(location.pathname) && confirm("Update README.md?")) {
 	var info = {
 		Username	: RegExp.$1,
 		Reponame	: RegExp.$2,
 		Branch		: RegExp.$3,
 		Path		: RegExp.$4,
 	},
-	repo = new Github({
+	github = new Github({
 	  token: GM_getValue("oauth", "OAUTH_TOKEN"),
 	  auth: "oauth",
-	}).getRepo(info.Username, info.Reponame);
+	}),
+	issu = github.getIssues(info.Username, info.Reponame),
+	repo = github.getRepo(info.Username, info.Reponame);
 	
 	function writeContent (content) {
-		if (confirm(content)) {
-			var f = info.Path + "/README.md";
-			repo.write(info.Branch, f, content, "Updated default " + f, function (err) {
-				if (err) {
-					alert(JSON.stringify(err));
-				} else {
-					alert(f + " updated sucessfully.");
-				}
-			});
+		var f = info.Path + "/README.md";
+		repo.write(info.Branch, f, content, "Updated default " + f, function (err) {
+			if (err) {
+				alert(JSON.stringify(err));
+			} else {
+				alert(f + " updated sucessfully.");
+			}
+		});
+	}
+
+	function processMeta (data) {
+		var obj = {},
+		re = /^\/\/\s+@([\w:]+)\s+(.+)/gim;
+		while (re.exec(data)) {
+			obj[RegExp.$1.toLowerCase()] = RegExp.$2;
 		}
+		return obj;
 	}
 
 	if ("scripts" == info.Path) {
 		repo.getTree(info.Branch + "?recursive=true", function (err, tree) {
 			var scripts = [];
 			for each (var file in tree) {
-				if (/^scripts\/([^_]+)_(\w+)\/((\w+)\.user\.js)$/.test(file.path)) {
+				if (/^scripts\/(([^_]+)_([\w-]+))\/((\w+)\.user\.js)$/.test(file.path)) {
 					scripts.push({
 						file	: file,
 						branch	: info.Branch,
-						group	: RegExp.$1,
-						name	: RegExp.$3,
-						sname	: RegExp.$4,
-						dir		: RegExp.$2.replace(/_+/g, " "),
+						group	: RegExp.$2,
+						name	: RegExp.$4,
+						sname	: RegExp.$5,
+						dir		: RegExp.$1,
+						sdir	: RegExp.$3,
+						fdir	: RegExp.$3.replace(/_+/g, " "),
 					});
 				}
 			}
@@ -78,20 +89,48 @@ if (/^\/(\w+)\/(\w+)\/tree\/(\w+)\/(.+)/.test(location.pathname)) {
 				return (a.group > b.group?1:-1);
 			});
 			
-			writeContent(Template.get(GM_getResourceText("templateList"), {
-				files	: scripts,
-			}));
+			(function recursive (ai) {
+				if (ai < scripts.length) {
+					repo.getBlob(scripts[ai].file.sha, function (err, data) {
+						scripts[ai].meta = processMeta(data);
+						recursive(++ai);
+					});
+				} else {
+					issu.list("state=all", function (err, issues) {
+						var labels = {};
+						for each (var issue in issues) {
+							for each (var label in issue.labels) {
+								if (!(label.name in labels)) {
+									labels[label.name] = {
+										open	: 0,
+										closed	: 0,
+										all		: 0,
+										label	: label.name,
+										flabel	: encodeURIComponent(label.name),
+									};
+								}
+								++labels[label.name][issue.state];
+								++labels[label.name].all;
+							}
+						}
+						for each (var s in scripts) {
+							var l = s.meta.name;
+							s.issues = labels[l] || {
+								open	: 0,
+								closed	: 0,
+								all		: 0,
+								label	: l,
+								flabel	: encodeURIComponent(l),
+							};
+						}
+						writeContent(Template.get(GM_getResourceText("templateList"), {
+							files	: scripts,
+						}));
+					});
+				}
+			}(0));
 		});
 	} else {
-		function processMeta (data) {
-			var obj = {},
-			re = /^\/\/\s+@([\w:]+)\s+(.+)/gim;
-			while (re.exec(data)) {
-				obj[RegExp.$1.toLowerCase()] = RegExp.$2;
-			}
-			return obj;
-		}
-
 		var files = Array.prototype.slice.apply(document.querySelectorAll("td.content a"));
 		for each (var file in files) {
 			if (/\.user\.js$/.test(file.textContent)) {
@@ -100,20 +139,33 @@ if (/^\/(\w+)\/(\w+)\/tree\/(\w+)\/(.+)/.test(location.pathname)) {
 				info.File = file.textContent.replace(/(?:\.\w{2,4}){1,2}$/, "");
 
 				repo.getBlob(info.Sha, function (err, data) {
-					writeContent(Template.get(GM_getResourceText("template"), {
-						meta	: processMeta(data),
-						info	: info,
-						file	: file,
-						raw		: "../../../raw/" + info.Branch + "/" + info.Path + "/" + file.textContent,
-						sshots	: files.filter(function (a) {
-									return /\.(?:jpg|png|gif)$/i.test(a.href);
-								}).map(function (a) {
-									return {
-										node	: a,
-										name	: a.textContent.replace(/\.\w+$/, ""),
-									};
-								}),
-					}));
+					var meta = processMeta(data);
+
+					issu.list("labels=" + meta.name, function (err, issues) {
+						writeContent(Template.get(GM_getResourceText("template"), {
+							meta	: meta,
+							info	: info,
+							file	: file,
+							issues	: issues.filter(function (a) {
+								for each (var label in a.labels) {
+									if (meta.name == label.name) {
+										return true;
+									}
+								}
+								return false;
+							}),
+							updated_at	: new Date().toISOString(),
+							raw		: "../../../raw/" + info.Branch + "/" + info.Path + "/" + file.textContent,
+							sshots	: files.filter(function (a) {
+										return /\.(?:jpg|png|gif)$/i.test(a.href);
+									}).map(function (a) {
+										return {
+											node	: a,
+											name	: a.textContent.replace(/\.\w+$/, ""),
+										};
+									}),
+						}));
+					});
 				});
 
 				break;
