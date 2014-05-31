17ad88ae5e7d9744dee2905585b27d5a2c2fbe56
w35l3y <w35l3y@brasnet.org>
2014-05-31 11:46:29 -0300

    Fixed Neopets_Medieval_Kings/28356

--- scripts/Neopets_Medieval_Kings/28356.user.js
+++ scripts/Neopets_Medieval_Kings/28356.user.js
@@ -19,15 +19,15 @@
 // @grant          GM_getResourceText
 // @icon           http://gm.wesley.eti.br/icon.php?desc=28356
 // @resource       includes http://pastebin.com/download.php?i=eArANXdm
-// @resource       meta http://userscripts.org/scripts/source/28356.meta.js
+// @resource       meta https://github.com/w35l3y/userscripts/raw/master/scripts/Neopets_Medieval_Kings/28356.user.js
 // @resource       i18n http://pastebin.com/download.php?i=ULrVTsSg
 // @resource       updaterWindowHtml http://pastebin.com/download.php?i=3gr9tRAT
 // @resource       updaterWindowCss http://pastebin.com/download.php?i=C1qAvAed
-// @require        http://userscripts.org/scripts/source/63808.user.js
-// @require        http://userscripts.org/scripts/source/56489.user.js
-// @require        http://userscripts.org/scripts/source/85618.user.js
-// @require        http://userscripts.org/scripts/source/87940.user.js
-// @require        http://userscripts.org/scripts/source/87942.user.js
+// @require        ../../includes/Includes_XPath/63808.user.js
+// @require        ../../includes/Includes_HttpRequest/56489.user.js
+// @require        ../../includes/Includes_Translate/85618.user.js
+// @require        ../../includes/Includes_I18n/87940.user.js
+// @require        ../../includes/Includes_Updater/87942.user.js
 // @require        http://pastebin.com/download.php?i=P6VTBRRK
 // @cfu:version    version
 // @history        4.0.0 Added <a href="http://userscripts.org/guides/773">Includes Checker</a>
