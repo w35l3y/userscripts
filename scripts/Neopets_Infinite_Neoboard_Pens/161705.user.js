8897a7d30d6128085df9ec6ebeabe28404f76182
w35l3y <w35l3y@brasnet.org>
2014-06-01 18:02:12 -0300

    Fixed Neopets_Infinite_Neoboard_Pens/161705
    Added some screenschots too

--- scripts/Neopets_Infinite_Neoboard_Pens/161705.user.js
+++ scripts/Neopets_Infinite_Neoboard_Pens/161705.user.js
@@ -22,15 +22,15 @@
 // @grant          GM_getResourceText
 // @icon           http://gm.wesley.eti.br/icon.php?desc=161705
 // @resource       includes http://pastebin.com/download.php?i=eArANXdm
-// @resource       meta http://userscripts.org/scripts/source/161705.meta.js
+// @resource       meta https://github.com/w35l3y/userscripts/raw/master/scripts/Neopets_Infinite_Neoboard_Pens/161705.user.js
 // @resource       i18n http://pastebin.com/download.php?i=ULrVTsSg
 // @resource       updaterWindowHtml http://pastebin.com/download.php?i=3gr9tRAT
 // @resource       updaterWindowCss http://pastebin.com/download.php?i=C1qAvAed
-// @require        http://userscripts.org/scripts/source/63808.user.js
-// @require        http://userscripts.org/scripts/source/56489.user.js
-// @require        http://userscripts.org/scripts/source/85618.user.js
-// @require        http://userscripts.org/scripts/source/87940.user.js
-// @require        http://userscripts.org/scripts/source/87942.user.js
+// @require        ../../includes/Includes_XPath/63808.user.js
+// @require        ../../includes/Includes_HttpRequest/56489.user.js
+// @require        ../../includes/Includes_Translate/85618.user.js
+// @require        ../../includes/Includes_I18n/87940.user.js
+// @require        ../../includes/Includes_Updater/87942.user.js
 // @require        http://pastebin.com/download.php?i=P6VTBRRK
 // @history        2.0.0 Added <a href="http://userscripts.org/guides/773">Includes Checker</a>
 // ==/UserScript==
