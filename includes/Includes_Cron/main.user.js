51507056a74c9d10be6a881ffae6ab819b9e8cd0
w35l3y <w35l3y@brasnet.org>
2015-02-16 23:38:13 -0300

    Create main.user.js

---
+++ includes/Includes_Cron/main.user.js
@@ -0,0 +1,269 @@
+// ==UserScript==
+// @name        Includes : Cron [BETA]
+// @namespace   http://gm.wesley.eti.br
+// @description Cron Function
+// @author      w35l3y
+// @email       w35l3y@brasnet.org
+// @copyright   2015+, w35l3y (http://gm.wesley.eti.br)
+// @license     GNU GPL
+// @homepage    http://gm.wesley.eti.br
+// @version     1.0.0
+// @language    en
+// @include     nowhere
+// @exclude     *
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_deleteValue
+// @icon        http://gm.wesley.eti.br/icon.php?desc=includes/Includes_Cron/main.user.js
+// ==/UserScript==
+
+/**************************************************************************
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+**************************************************************************/
+
+// http://infocenter.sybase.com/help/index.jsp?topic=/com.sybase.infocenter.dc01963.0510/doc/html/psh1360346725201.html
+var Cron = function (id, current) {
+	var diff = (current?current.valueOf() - Date.now():0),
+	_debug = function () {
+		false && console.debug.apply(console, arguments);
+	},
+	_currentDate = function () {
+		var v = new Date();
+		diff && v.setUTCMilliseconds(v.getUTCMilliseconds() + diff);
+		return v;
+	},
+	createdAt = _currentDate(),
+	nextAtKey = id + "-nextAt",
+	nextAt = JSON.parse(GM_getValue(nextAtKey, "{}")),
+	timer,
+	_type = {
+		SECOND	: 0,
+		MINUTE	: 1,
+		HOUR	: 2,
+		DAY		: 3,
+		MONTH	: 4,
+		WEEKDAY	: 5,
+		YEAR	: 6,
+
+		NONE	: 0,
+
+		ASTERISK: 1,
+		QUESTION: 2,
+
+		HASH	: 1,
+		SLASH	: 2,
+	},
+	intervals = [
+		[0, 59],	// second
+		[0, 59],	// minute
+		[0, 23],	// hour
+		[1, 31],	// day
+		[1, 12],	// month
+		[0, 6],	// weekday
+		[0, 0],	// year
+	],
+	methods = [
+		[60, "UTCSeconds"],
+		[60, "UTCMinutes"],
+		[24, "UTCHours"],
+		[31, "UTCDate"],	// under test [Date.UTC(createdAt.getUTCFullYear(), 1 + createdAt.getUTCMonth(), 0, 0, 0, 0, 0).getUTCDate(), "UTCDate"],
+		[12, "UTCMonth"],
+		[7, "UTCDay"],
+		[0, "UTCFullYear"],	// under test
+	],
+	Task = function (obj) {
+		var _current = _currentDate(),
+		map = {
+					// s m h D M d Y
+			reboot	: "? ? ? ? ? * ?",
+			yearly	: "0 0 0 1 1 * *",
+			monthly	: "0 0 0 1 * * *",
+			weekly	: "0 0 0 * * 0 *",
+			daily	: "0 0 0 * * * *",
+			midnight: "0 0 0 * * * *",
+			hourly	: "0 0 * * * * *",
+		};
+		Object.defineProperties(this, {
+			id	: {
+				value	: obj.id,
+			},
+			interval	: {
+				value	: ((obj.interval || "*").replace(/^@(\w+)$/, function ($0, $1) {
+					return ($1 in map?map[$1]:$0);
+				}) + " * * * * * *").trim().split(/\s+/g).slice(0, 7).map(function (data, index, array) {
+					if (/^([\*\?]|\d+(?:-\d+)?(?:,\d+(?:-\d+)?)*)(?:([\#\/])(\d+))?$/.test(data)) {
+						var type = 1 + "#/".indexOf(RegExp.$2 || undefined);
+
+						if (_type.HASH != type || _type.WEEKDAY == index) {
+							var step = parseInt(RegExp.$3, 10) || 1,
+							currentValue = _current["get" + methods[index][1]](),
+							pInterval = intervals[index];
+
+							return {
+								step	: step,
+								type	: type,
+								wildcard: 1 + "*?".indexOf(RegExp.$1 || undefined),
+								values	: RegExp.$1.replace("?", function () {
+									return currentValue + (currentValue % step);
+								}).replace("*", pInterval.join("-")).replace(/(\d+)-(\d+)/g, function ($0, $1, $2) {
+									var isMonth = (_type.MONTH == index),
+									opts = [parseInt($1, 10) - isMonth + (obj.relative?currentValue % step:0)];
+									for (var ai = opts[0] + step, at = parseInt($2, 10) - isMonth;ai <= at;ai += step) {
+										opts.push(ai);
+									}
+									return opts.join(",");
+								}).split(",").map(function (data) {
+									return parseInt(data, 10);	// parse numbers
+								}).sort(function (a, b) {
+									return (a > b?1:-1);	// asc
+								}).filter(function (data, index, array) {
+									return data != array[index - 1] && (_type.YEAR == index || pInterval[0] <= data && data <= pInterval[1]);	// repeated values and out of bound
+								}),
+							};
+						}
+					}
+
+					throw "Malformed or not supported parameter: " + data + " " + index;
+				}),
+			}
+		});
+		
+		this.ready = function (date) {
+			return this.next() <= date && this.interval.every(function (data, index, array) {
+				if (_type.WEEKDAY == index && _type.HASH == data.type) {
+					throw "Not implemented yet";
+				} else {
+					return (_type.ASTERISK == data.wildcard
+						|| 0 <= data.values.indexOf((_type.MONTH == index) + date["get" + methods[index][1]]()));
+				}
+			});
+		};
+		
+		this.execute = function (cb) {
+			var _this = this,
+			pUpdate = function () {
+				_this.update(_currentDate());
+				cb(_this);
+			},
+			c = _currentDate();
+
+			if (this.ready(c)) {
+				_debug("2 READY", obj.id, c);
+				if (obj.command(pUpdate)) {
+					pUpdate();
+				} else {
+					_debug("No synchronous response (possibly asynchronous)");
+				}
+			} else {
+				_debug("2 WAIT", obj.id, c);
+				pUpdate();
+			}
+		};
+		
+		this.update = function (date) {
+			date.setUTCMilliseconds(1000);
+
+			for (var index = 0, at = this.interval.length;index < at;++index) {
+				var method = methods[index],
+				pInterval = this.interval[index],
+				tInterval = pInterval.values,
+				value = date["get" + method[1]](),
+				tmp = [0, method[0]];
+				for (var bi = 0, bt = tInterval.length;bi < bt;++bi) {
+					if (tInterval[bi] >= value) {
+						tmp = [bi, 0];
+						break;
+					}
+				}
+				
+				if (_type.WEEKDAY == index) {
+					date.setUTCDate(date.getUTCDate() + tInterval[tmp[0]] + tmp[1] - value);
+				} else if (_type.YEAR != index || 0 < tInterval[tmp[0]]) {
+					date["set" + method[1]](tInterval[tmp[0]] + tmp[1]);
+				}
+			}
+
+			_debug("3 NEXT", obj.id, date);
+			nextAt[obj.id] = date.valueOf();
+			GM_setValue(nextAtKey, JSON.stringify(nextAt));
+		};
+
+		this.next = function () {
+			return nextAt[obj.id] || 0;//new Date().getUTCMilliseconds();
+		};
+
+		_debug(this);
+	},
+	tasks = [],
+	_add = function (o) {
+		var pInterval = o.next();
+		for (var index = 0, at = tasks.length;index < at && pInterval >= tasks[index].next();++index) {}
+
+		_debug("0 ADD", o.id, pInterval, index);
+		tasks.splice(index, 0, o);	// inserts 'o' at position 'index'
+
+		_updateTimer();
+		
+		return o;
+	},
+	_updateTimer = function () {
+		clearTimeout(timer);
+
+		if (tasks.length) {
+			var cd = _currentDate(),
+			n = tasks[0].next(),
+			wait = Math.max(Math.min(n - cd, Math.pow(2, 31)), 0);
+			_debug("1 TIMER", tasks[0].id, cd, n, n - cd, wait);
+			timer = setTimeout(function () {
+				tasks.shift().execute(_add);
+			}, wait);
+		}
+	};
+	
+	this.add = function (obj) {
+		return _add(new Task(obj));
+	};
+
+	this.rm = function (o) {
+		var index = -1;
+		if (typeof o == "string") {
+			for (var ai = 0, at = tasks.length;ai < at;++ai) {
+				if (o == tasks[ai].id) {
+					index = ai;
+					break;
+				}
+			}
+		} else {
+			index = tasks.indexOf(o);
+		}
+
+		var removed = tasks.splice(index, 1)[0];
+		if (!index) {
+			_updateTimer();
+		}
+
+		return removed;
+	};
+
+	this.reset = function () {
+		nextAt = {};
+		GM_deleteValue(nextAtKey);
+
+		_updateTimer();
+	};
+	
+	_debug("0 DIFF", diff);
+};
