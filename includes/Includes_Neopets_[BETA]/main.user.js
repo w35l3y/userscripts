07d0c01a65647a0069acbdf0420f2c8cf2054f05
w35l3y <w35l3y@brasnet.org>
2015-03-29 23:16:41 -0300

    Added Random Events

--- includes/Includes_Neopets_[BETA]/main.user.js
+++ includes/Includes_Neopets_[BETA]/main.user.js
@@ -7,7 +7,7 @@
 // @copyright   2015+, w35l3y (http://gm.wesley.eti.br)
 // @license     GNU GPL
 // @homepage    http://gm.wesley.eti.br
-// @version     1.2.11
+// @version     1.2.12
 // @language    en
 // @include     nowhere
 // @exclude     *
@@ -178,7 +178,11 @@ var Neopets = function (doc) {
 				var np = _n("id('header')//td/a[contains(@href, 'inventory')]/text()"),
 				_refck = _s(".//*[(@name = '_ref_ck' or @name = 'ck') and string-length(@value) = 32]/@value") || (/_ref_ck=(\w{32})/.test(_s(".//*[contains(@href, '_ref_ck')]/@href"))?RegExp.$1:""),
 				listen = [
-					["events", _b(".//div[@class = 'inner_wrapper2']|.//table[@width = '400']")],
+					["events", _b(".//div[@class = 'inner_wrapper2']|.//table[@width = '400']"), function (obj) {
+						return xpath(".//div[@class = 'inner_wrapper2']|.//table[@width = '400']").map(function (item) {
+							return item.outerHTML;
+						});
+					}],
 				],
 				data = {
 					error	: _b(".//img[@class = 'errorOops']|id('oops')"),
@@ -191,6 +195,9 @@ var Neopets = function (doc) {
 
 				for (var ai in listen) {
 					if (listen[1] && listen[0] in _listeners) {
+						if (listen[2]) {
+							data.result = listen[2](data);
+						}
 						for (var bi = 0, bt = _listeners[listen[0]].length;bi < bt;++bi) {
 							_listeners[listen[0]][bi](data);
 						}
@@ -250,7 +257,7 @@ var Neopets = function (doc) {
 		},
 		delay		: {
 			writable: true,
-			value	: [734, 463],
+			value	: [834, 363],
 		},
 		nc			: {
 			get		: function () {
