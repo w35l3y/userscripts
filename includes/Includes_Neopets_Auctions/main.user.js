d95e06e9c2135cd44781c12052734b7708f258d0
Jacob Kossman <jacob.kossman@gmail.com>
2016-04-21 19:36:01 -0700

    Added info

--- includes/Includes_Neopets_Auctions/main.user.js
+++ includes/Includes_Neopets_Auctions/main.user.js
@@ -20,7 +20,7 @@ var Auctions = function(page){
     var _post = function(data, cb){
 		page.request({
 			method	: "post",
-			action	: "http://www.neopets.com/auctions.phtml?type=placebid",
+			action	: "http://www.neopets.com/auctions.phtml",
 			referer	: referer,
 			data	: data,
 			delay	: true,
@@ -37,8 +37,41 @@ var Auctions = function(page){
         });
     };
     
-    this.test = function(cb){
-        _get({ "auction_counter" : 40 },function(o){
+    this.info = function(obj){
+        _get({ "type" : "bids", "auction_id" : obj.auction_id },function(o){
+            if(xpath(".//td[@class = 'content']//b[contains(text(),'Oops')]/text()",o.body).length > 0){
+                o.error = true;
+                o.errmsg = xpath("string(.//td[@class = 'content']//b[contains(text(),'Oops')]/text())",o.body) + xpath("string(.//td[@class = 'content']//b[contains(text(),'Oops')]/../text())",o.body);         
+            }else{
+                // get info
+                o.info = {};
+                if(xpath("string(.//td[@class = 'content']//center[contains(text(),'This auction is closed, you cannot place any more bids!')])",o.body).length > 0){
+                    o.info.closed = true;
+                }else{
+                    o.info.closed = false;                    
+                }
+                o.info.id = parseInt(obj.auction_id,10);
+                o.info.item = xpath("string(.//td[@class = 'content']//b[contains(text(),'(owned by')]/text())",o.body).split("(")[0].trim();
+                o.info.owner = xpath("string(.//td[@class = 'content']//b[contains(text(),'(owned by')]/text())",o.body).split("(")[1].replace(")","").replace("owned by ","").trim(); 
+                if(xpath(".//td[@class = 'content']//center[2]/table[@cellspacing = '0']/tbody/tr[position()>1]",o.body).length == 0){
+                    o.info.bids = false;
+                }else{
+                    o.info.bids = xpath(".//td[@class = 'content']//center[2]/table[@cellspacing = '0']/tbody/tr[position()>1]",o.body).map(function(bid){
+                        return {
+                            bidder : xpath("string(td[1]/text())",bid).trim(),
+                            amount : parseInt(xpath("string(td[2]/b/text())",bid).replace(/,/g,""),10),
+                            when   : xpath("string(td[3]/text())",bid)
+                        }
+                    });
+                }
+            }
+            
+            obj.callback(o);
+        });        
+    }
+    
+    this.page = function(obj){
+        _get({ "auction_counter" : obj.page },function(o){
             if(xpath(".//td[@class = 'content']//b[contains(text(),'Oops')]/text()",o.body).length > 0){
                 o.error = true;
                 o.errmsg = xpath("string(.//td[@class = 'content']//b[contains(text(),'Oops')]/text())",o.body) + xpath("string(.//td[@class = 'content']//b[contains(text(),'Oops')]/../text())",o.body);         
@@ -56,7 +89,7 @@ var Auctions = function(page){
                 });
             }
             
-            cb(o);
+            obj.callback(o);
         });
-    };  
+    };
 };
