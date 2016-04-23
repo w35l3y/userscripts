21ec6d0ab8dfd7baeb509a691a2bc32689442607
w35l3y <w35l3y@brasnet.org>
2016-04-23 15:27:18 -0300

    Added new functionalities
    New: parse, bid, search, leading, mine
    Changed: info/bids, page/auctions

--- includes/Includes_Neopets_Auctions/main.user.js
+++ includes/Includes_Neopets_Auctions/main.user.js
@@ -1,95 +1,226 @@
 // ==UserScript==
-// @name        Neopets : Includes : Auctions
+// @name        Includes : Neopets : Auctions
 // @namespace   github.com
+// @description Auctions Function
+// @author      jacobkossman
+// @email       jacob.kossman@gmail.com
+// @copyright   2016+, jacobkossman
+// @version     1.0.0
+// @language    en
 // @include     nowhere
 // @exclude     *
-// @version     1
+// @grant       GM_getValue
+// @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
-// @grant       GM_getResourceText
-// @require     http://code.jquery.com/jquery-latest.js
-// @require     https://github.com/knadh/localStorageDB/raw/master/localstoragedb.min.js
+// @require     https://github.com/w35l3y/localStorageDB/raw/master/localstoragedb.js
 // @require     https://github.com/w35l3y/userscripts/raw/master/includes/Includes_XPath/63808.user.js
 // @require     https://github.com/w35l3y/userscripts/raw/master/includes/Includes_HttpRequest/56489.user.js
-// @require     https://github.com/w35l3y/userscripts/raw/master/includes/Includes_Neopets_%5BBETA%5D/main.user.js?2387
+// @require     https://github.com/w35l3y/userscripts/raw/master/includes/Includes_Neopets_[BETA]/main.user.js
+// @contributor w35l3y
 // ==/UserScript==
 
-// auctions.phtml?type=placebid
-// { _ref_ck : neo.ck, amount : 245000, auction_id : auctionid }
-
-var Auctions = function(page){
-    var _post = function(data, cb){
+var Auctions = function (page) {
+    var _this = this,
+	_response = function (xhr, p) {
+		Object.defineProperties(xhr, {
+			response	: {
+				get	: function () {
+					return p(xhr)
+				}
+			}
+		});
+	},
+	_post_genie = function (data, p, cb) {
+		page.request({
+			method	: "post",
+			action	: "http://www.neopets.com/genie.phtml",
+			referer	: "http://www.neopets.com/genie.phtml",
+			data	: data,
+			delay	: true,
+            callback : function (xhr) {
+           	    p && _response(xhr, p);
+           	    cb(xhr);
+		    }
+		});
+    },
+	_post = function (data, p, cb) {
 		page.request({
 			method	: "post",
 			action	: "http://www.neopets.com/auctions.phtml",
-			referer	: referer,
+			referer	: "http://www.neopets.com/auctions.phtml?type=bids&auction_id=" + data.auction_id,
 			data	: data,
+			ck		: "_ref_ck",
 			delay	: true,
-			callback: cb
+            callback : function (xhr) {
+           	    p && _response(xhr, p);
+           	    cb(xhr);
+		    }
 		});
     },
-    _get = function(data,cb){
+    _get = function (data, p, cb) {
         page.request({
-           method   : "get",
-           action   : "http://www.neopets.com/auctions.phtml",
-           data     : data,
-           delay    : true,
-           callback : cb
+            method   : "get",
+            action   : "http://www.neopets.com/auctions.phtml",
+            data     : data,
+            delay    : true,
+            callback : function (xhr) {
+				var oops = xpath("string(.//td[@class = 'content']/center[2]/p[b and not(img) and text()])", xhr.body);
+				if (oops) {
+					xhr.error = true;
+					xhr.errmsg = oops;
+				}
+           	    p && _response(xhr, p);
+           	    cb(xhr);
+		    }
         });
     };
-    
-    this.info = function(obj){
-        _get({ "type" : "bids", "auction_id" : obj.auction_id },function(o){
-            if(xpath(".//td[@class = 'content']//b[contains(text(),'Oops')]/text()",o.body).length > 0){
-                o.error = true;
-                o.errmsg = xpath("string(.//td[@class = 'content']//b[contains(text(),'Oops')]/text())",o.body) + xpath("string(.//td[@class = 'content']//b[contains(text(),'Oops')]/../text())",o.body);         
-            }else{
-                // get info
-                o.info = {};
-                if(xpath("string(.//td[@class = 'content']//center[contains(text(),'This auction is closed, you cannot place any more bids!')])",o.body).length > 0){
-                    o.info.closed = true;
-                }else{
-                    o.info.closed = false;                    
-                }
-                o.info.id = parseInt(obj.auction_id,10);
-                o.info.item = xpath("string(.//td[@class = 'content']//b[contains(text(),'(owned by')]/text())",o.body).split("(")[0].trim();
-                o.info.owner = xpath("string(.//td[@class = 'content']//b[contains(text(),'(owned by')]/text())",o.body).split("(")[1].replace(")","").replace("owned by ","").trim(); 
-                if(xpath(".//td[@class = 'content']//center[2]/table[@cellspacing = '0']/tbody/tr[position()>1]",o.body).length == 0){
-                    o.info.bids = false;
-                }else{
-                    o.info.bids = xpath(".//td[@class = 'content']//center[2]/table[@cellspacing = '0']/tbody/tr[position()>1]",o.body).map(function(bid){
-                        return {
-                            bidder : xpath("string(td[1]/text())",bid).trim(),
-                            amount : parseInt(xpath("string(td[2]/b/text())",bid).replace(/,/g,""),10),
-                            when   : xpath("string(td[3]/text())",bid)
-                        }
-                    });
-                }
-            }
-            
-            obj.callback(o);
-        });        
-    }
-    
-    this.page = function(obj){
-        _get({ "auction_counter" : obj.page },function(o){
-            if(xpath(".//td[@class = 'content']//b[contains(text(),'Oops')]/text()",o.body).length > 0){
-                o.error = true;
-                o.errmsg = xpath("string(.//td[@class = 'content']//b[contains(text(),'Oops')]/text())",o.body) + xpath("string(.//td[@class = 'content']//b[contains(text(),'Oops')]/../text())",o.body);         
-            }else{
-                o.items = xpath(".//td[@class = 'content']//table[@cellpadding = '3']//tbody/tr[position()>1]",o.body).map(function(row){
-                    return {
-                        id           : parseInt(xpath("string(td[3]/a/@href)",row).split("=")[2],10),
-                        name         : xpath("string(td[3]/a/text())",row),
-                        owner        : xpath("string(td[4]/font/text())",row),
-                        timeleft     : xpath("string(td[5]/b/font/text())",row),
-                        lastbid      : parseInt(xpath("string(td[6]/b/text())",row),10),
-                        currentprice : parseInt(xpath("string(td[7]/b/text())",row),10),
-                        lastbidder   : xpath("string(td[8]/font/text())",row)
-                    }
-                });
-            }
-            
-            obj.callback(o);
-        });
+
+	this.parse = function (type, doc) {
+		if (!doc) {
+			doc = page.document;
+		}
+
+		var _n = function (v, d) {
+			return parseInt(xpath(v, d || doc).replace(/\D+/g, ""), 10);
+		};
+
+		switch (type) {
+			case "auctions":
+			case "genie":
+				return {
+					list	: xpath(".//td[@class = 'content']//table//tbody/tr[td[8]]", doc).map(function (row) {
+						return {
+							id           : /\bauction_id=(\d+)/.test(xpath("string(td[3]/a/@href)", row)) && parseInt(RegExp.$1, 10),
+							item         : {
+								image        : xpath("string(./td[2]/a/img/@src)", row),
+								name         : xpath("string(./td[3]/a/text())", row),
+							},
+							owner        : xpath("string(./td[4]/font/text())", row),
+							nf_only		 : xpath("boolean(./td[4]/b)", row),
+							timeleft     : xpath("string(./td[5]/b/font/text())", row),
+							currentprice : _n("string(./td[7]/b/text())", row),
+							closed       : false,
+							lastbid      : _n("string(./td[6]/b/text())", row),
+							lastbidder   : (xpath("boolean(./td[8]/a/img)", row)?xpath("string(./td[8]/font/text())", row):""),
+						}
+					})
+				};
+			case "bids":
+				var item = /([^\(]+).+?(\w+)\)/.test(xpath("string(.//td[@class = 'content']//p[1]/b)", doc)) && [RegExp.$1, RegExp.$2] || [""],
+				o = {
+					id		: _n("string(.//td[@class ='content']/p[2]/b)"),
+					item	: {
+						name	: item[0].trim(),
+						image	: xpath("string(.//td[@class = 'content']//p[1]/img/@src)", doc),
+						description: xpath("string(.//td[@class = 'content']/center[2]/p[1]/text())", doc),
+					},
+					owner	: item[1],
+					nf_only	: xpath("boolean(.//td[@class = 'content']/p/span/b)", doc),
+					timeleft: xpath("string(.//td[@class = 'content']/center[2]/text())", doc).trim(),
+					increment: _n("string(.//td[@class = 'content']/p[3]/b[position() = last()])"),
+					currentprice	: _n("string(.//td[@class = 'content']//form//input[@name = 'amount']/@value)"),
+					list	: xpath(".//td[@class = 'content']//table/tbody/tr[td[1]/a/img]", doc).map(function (bid) {
+						return {
+							bidder : xpath("string(td[1])", bid).trim(),
+							amount : parseInt(xpath("string(td[2]/b)", bid).replace(/\D+/g, ""), 10),
+							when   : xpath("string(td[3])", bid).trim()
+						};
+					}),
+				};
+				o.closed = !o.currentprice;
+				o.lastbid = (o.list.length?o.list[0].amount:(o.currentprice - o.increment));
+				o.lastbidder = (o.list.length?o.list[0].bidder:"");
+				
+				return o;
+			case "placebid":
+				var msg = xpath(".//td[@class = 'content']/center[2]/p/font", doc)[0];
+
+				return msg?{
+					success	: ("green" == msg.getAttribute("color"))
+					text	: msg.textContent.trim()
+				}:{
+					success	: false,
+					text	: ""
+				};
+			default:
+				throw "Unknown type: " + type
+		}
+	};
+
+	this.bid = function (obj) {
+		_post({
+			type		: "placebid",
+			auction_id	: obj.id || obj.auction.id
+			amount		: obj.value || obj.auction.currentprice,
+		}, function (o) {
+			return _this.parse("placebid", o.body);
+		}, obj.callback);
+	};
+
+    this.bids = function (obj) {
+        _get({
+			"type"			: "bids",
+			"auction_id"	: obj.id || obj.auction.id
+		}, function (o) {
+			return _this.parse("bids", o.body);
+        }, obj.callback);
     };
+
+    this.auctions = function (obj) {
+		var list = [];
+		(function recursive (page) {
+			_get(Object.assign({
+				"auction_counter"	: 20 * page,
+			}, obj.data), null, function (o) {
+				var result = _this.parse("auctions", o.body);
+				Array.prototype.push.apply(list, result.list);
+
+				if (obj.all && 20 == result.list.length) {
+					recursive(++page);
+				} else {
+					_response(o, function () {
+						return {
+							list	: list
+						}
+					});
+
+					obj.callback(o);
+				}
+			});
+		}(obj.page || 0));
+    };
+
+	this.mine = function (obj) {
+		_this.auctions({
+			all		: true,
+			data	: {
+				show	: "mine"
+			},
+			callback	: obj.callback
+		});
+	};
+
+	this.leading = function (obj) {
+		_this.auctions({
+			all		: true,
+			data	: {
+				type	: "leading"
+			},
+			callback	: obj.callback
+		});
+	};
+
+	this.search = function (obj) {
+		_post_genie(Object.assign(("username" == obj.type?{
+			type	: "find_user",
+			auction_username: obj.value || page.username || "",
+		}:{
+			type		: "process_genie",
+			auctiongenie: obj.value || "",
+			criteria	: "exact",
+			exclude_nf_only: "On",
+		}), obj.data), function (o) {
+			return _this.parse("genie", o.body);
+		}, obj.callback);
+	};
 };
